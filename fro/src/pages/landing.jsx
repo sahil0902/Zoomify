8e36adcfb9c73535683ca08a7bcfd64f2efae804
Muhammad Sahil <sahil@Ss-MacBook-Air.local>
2024-10-29 00:08:05 +0000

    imporved landing page

--- fro/src/pages/landing.jsx
+++ fro/src/pages/landing.jsx
@@ -1,32 +1,43 @@
-import React from 'react'
-import "../App.css"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import "../App.css";
+import { Link } from 'react-router-dom';
+import './Landing.css';
+
 export default function LandingPage() {
   return (
     <div className='landingPageContainer'>
-    <nav>
+      <nav>
         <div className='navHeader'><h2>Zoomify</h2></div>
         <div className='navlist'>
-          <p>Join as Guest</p>
-          <p>Register</p>
+          <div className="comingSoon">
+            <p className="glowingText">Coming Soon</p>
+          </div>
+          <Link to="/auth" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <p>Join as Guest</p>
+          </Link>
+          <Link to="/auth" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <p>Register</p>
+          </Link>
+          <Link to="/auth" style={{ textDecoration: 'none', color: 'inherit' }}>
+            <div role='button'>
+              <p>Login</p>
+            </div>
+          </Link>
+        </div>
+      </nav>
+
+      <div className="landingMainContainer">
+        <div>
+          <h1><span style={{ color: "#8CAAD2" }}>Cherish the connections that matter most</span></h1>
+          <p>Cover your distance with Zoomify!</p>
           <div role='button'>
-            <p>Login</p>
+            <Link to={"/auth"} style={{ color: "white" }}>Get Started</Link>
           </div>
         </div>
-    </nav>
-
-    <div className="landingMainContainer">
-        <div>
-            <h1><span style={{color:"#8CAAD2"}}>Cherish the connections that matter most</span></h1>
-       <p>Cover your distance with Zoomify!</p>
-       <div role='button'>
-        <Link to={"/auth"} style={{color:"white"}}>Get Started</Link>
-       </div>
-        </div>
-               <div className='landingImages'>
+        <div className='landingImages'>
           <img src="/svgs/meeting1.svg" alt="Media" className="responsiveImage" />
         </div>
+      </div>
     </div>
-    </div>
-  )
+  );
 }
