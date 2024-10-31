2df7fa061c9cdae75c8e05f2bb0a0d18d6d3c219
Muhammad Sahil <sahil@Ss-MacBook-Air.local>
2024-10-31 10:08:29 +0000

    updated frontend

--- fro/src/environment.js
+++ fro/src/environment.js
@@ -1,4 +1,3 @@
 let IS_PRODUCTION = true;
-const server =  'http://localhost:8000';
-//IS_PRODUCTION ? 'https://zoomifybackend-mry1.onrender.com' :
+const server = IS_PRODUCTION ? 'https://zoomifybackend-mry1.onrender.com' : 'http://localhost:8000';
 export default server;
